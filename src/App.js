0aa58ab18be44bbf00b014b8b1bef6992d0e4a55
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-07-17 16:31:21 -0600

    Working on calling from stateful components to stateless components

--- src/App.js
+++ src/App.js
@@ -28,16 +28,40 @@ class App extends Component {
     })
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Pietro', age: 34},
+        {name: 'Julianne', age: 23},
+        {name: 'Vicious', age: 2700}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Cars model="X90" description="Lightning fast!">You should take it for a spin.</Cars>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name="Karla" age="21" />
-        <Person name="Jessica" age="26" />
-        <Children name="Natasha">Explore the World</Children>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler}><b>So you're wondering how I've gotten to this age. Vampires are a lowly form of demon. I'm way above that!</b></Person>
+
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}  />
+
+        <Person
+          name="Karla"
+          age="21" />
+
+          {/* <Cars model="X90" description="Lightning fast!">You should take it for a spin.</Cars> */}
+        {/* <Children name="Natasha">Explore the World</Children> */}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,8 @@ const Person = (props) =>{
   return(
     <div>
       <h1>Hello, My name is {props.name} and I'm {props.age} years old</h1>
+      <p>{props.children}</p>
+      <input type='text' />
     </div>
   )
 }
