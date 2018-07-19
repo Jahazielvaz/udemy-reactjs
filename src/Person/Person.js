84b89a90b4c8630f9bd1468205a148e70942efcf
Jahaziel <jahaziel.vaz@gmail.com>
2018-07-18 23:58:33 -0600

    working on 2 way binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const Person = (props) =>{
   return(
-    <div>
-      <h1 onClick={props.click}>Hello, My name is {props.name} and I'm {props.age} years old</h1>
+    <div className="Person">
+      <h1 onClick={props.click}>Hello, My name is {props.name} and Im {props.age} years old</h1>
       {/* <p onClick={props.click}>{props.children}</p> */}
-      <input type='text' onChange={props.changed} />
+      <input type='text' onChange={props.changed} value={props.name} />
     </div>
   )
 }
